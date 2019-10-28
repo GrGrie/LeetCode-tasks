b7f69e770e591df7648c1af5c52e2964baf1d14b
GrGrie <gsgrechkin@gmail.com>
2019-10-28 16:02:20 +0300

    Updated Task1051
    Added task description inside the file.
    Also added 'import' part.

---
+++ Task1051.java
@@ -0,0 +1,12 @@
+/* Students are asked to stand in non-decreasing order of heights for an annual photo.
+
+Return the minimum number of students not standing in the right positions.  
+(This is the number of students that must move in order for all students to be standing in non-decreasing order of height.)*/
+
+import java.util.Scanner;
+
+public class Task1051{
+    public static void main(String[] args) {
+        
+    }
+}
