a3f49a67d2274e1d82b20de3240e93440d459aa1
GrGrie <gsgrechkin@gmail.com>
2019-10-29 22:25:01 +0300

    Created Task13.java
    It is empty fot a bit

---
+++ Task13.java
@@ -0,0 +1,17 @@
+/*
+    Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+    I can be placed before V (5) and X (10) to make 4 and 9. 
+    X can be placed before L (50) and C (100) to make 40 and 90. 
+    C can be placed before D (500) and M (1000) to make 400 and 900.
+    Given a roman numeral, convert it to an integer. Input is guaranteed to be within the range from 1 to 3999.
+*/
+import java.util.Scanner;
+public class Task13{
+    public int romanToInt(String s){
+
+    }
+    
+    public static void main(String[] args) {
+        System.out.println(romanToInt(new Scanner(System.in).nextLine()));
+    }
+}
