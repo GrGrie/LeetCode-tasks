e9ea4de981936934f24871c3918001b70d347388
GrGrie <gsgrechkin@gmail.com>
2019-10-30 13:05:03 +0300

    Update Task13.java
    My final cut of program.

--- Task13.java
+++ Task13.java
@@ -7,11 +7,29 @@
 */
 import java.util.Scanner;
 public class Task13{
-    public int romanToInt(String s){
-
+    public static int romanToInt(String s){
+        int summ = 0;
+        if(s.contains("IV")) summ -= 2;
+        if(s.contains("IX")) summ -= 2;
+        if(s.contains("XL")) summ -= 20;
+        if(s.contains("XC")) summ -= 20;
+        if(s.contains("CD")) summ -= 200;
+        if(s.contains("CM")) summ -= 200;
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == 'I') summ++;
+            else if(s.charAt(i) == 'V') summ += 5;
+            else if(s.charAt(i) == 'X') summ += 10;
+            else if(s.charAt(i) == 'L') summ += 50;
+            else if(s.charAt(i) == 'C') summ += 100;
+            else if(s.charAt(i) == 'D') summ += 500;
+            else if(s.charAt(i) == 'M') summ += 1000;
+        }
+        return summ;
     }
     
     public static void main(String[] args) {
-        System.out.println(romanToInt(new Scanner(System.in).nextLine()));
+        Scanner input = new Scanner(System.in);
+        System.out.println(romanToInt(input.nextLine()));
+        input.close();
     }
 }
