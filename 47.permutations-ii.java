70a182ceb05bfa118febc401c5ca2a0cb86ea5c9
GrGrie <gsgrechkin@gmail.com>
2022-05-12 15:36:17 +0800

    Installed Leetcode extension to VS code, trying it out with PotD

---
+++ 47.permutations-ii.java
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=47 lang=java
+ *
+ * [47] Permutations II
+ */
+
+// @lc code=start
+class Solution {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        boolean[] isUsed = new boolean[nums.length];
+        int[] permutationArray = new int[nums.length];
+        
+    }
+}
+// @lc code=end
+
