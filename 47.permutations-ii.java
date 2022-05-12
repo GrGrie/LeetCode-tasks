7f7dc66dd5da8f2a8ce905f41734c35d273ff0bc
GrGrie <gsgrechkin@gmail.com>
2022-05-12 15:43:04 +0800

    Added comment about my struggling

--- 47.permutations-ii.java
+++ 47.permutations-ii.java
@@ -7,9 +7,16 @@
 // @lc code=start
 class Solution {
     public List<List<Integer>> permuteUnique(int[] nums) {
+
+        /*
+        After ~15 mins of figuring out how to solve this problem haven't came up
+        with the good idea of how to do it. Watched a YouTube video by NeetCode (not the code itself),
+        after that whatched a video about HashMap cuz I have a pretty weak understanding
+        of Collection at this point. Now going to try ad do stuff
+        */
         boolean[] isUsed = new boolean[nums.length];
         int[] permutationArray = new int[nums.length];
-        
+
     }
 }
 // @lc code=end
