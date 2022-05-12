55e64d379d35a09bd6a97d042da912f5c3f73ecb
GrGrie <gsgrechkin@gmail.com>
2022-05-12 15:58:09 +0800

    Made a HashMap :)

--- 47.permutations-ii.java
+++ 47.permutations-ii.java
@@ -5,6 +5,10 @@
  */
 
 // @lc code=start
+import java.util.List;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 class Solution {
     public List<List<Integer>> permuteUnique(int[] nums) {
 
@@ -14,9 +18,27 @@ class Solution {
         after that whatched a video about HashMap cuz I have a pretty weak understanding
         of Collection at this point. Now going to try ad do stuff
         */
-        boolean[] isUsed = new boolean[nums.length];
-        int[] permutationArray = new int[nums.length];
+        
+        // I will add all the numbers of nums[] here:
+        HashMap<Integer, Integer> numbersToPermutate = new HashMap<>();
+        // Outputted sublist:
+        List<Integer> subList = new ArrayList<>();
+        // Answer:
+        List<List<Integer>> answer = new ArrayList<>();
 
+        // Let's add nums to HashMap:
+        for(int i : nums){
+            // If the number is new, add it to HashMap, otherwise increase value (amount of repetetives) by 1
+            if(!numbersToPermutate.containsKey(nums[i]))
+                numbersToPermutate.put(nums[i], 1);
+            else
+                numbersToPermutate.replace(nums[i], numbersToPermutate.get(nums[i]) + 1);
+        }
+
+        
+
+
+        return answer;
     }
 }
 // @lc code=end
