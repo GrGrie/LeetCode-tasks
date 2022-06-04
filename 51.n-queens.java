3e022b004d1fdc80e0df6b32b01fc168389c1195
GrGrie <gsgrechkin@gmail.com>
2022-06-04 16:19:53 +0800

    Now it tries to solve the problem columnwise, but does not backtrack, so I only get the first unsuccessfull attempt. Working on it now

--- 51.n-queens.java
+++ 51.n-queens.java
@@ -8,6 +8,7 @@
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Stack;
 
 class Solution {
     public List<List<String>> solveNQueens(int n) {
@@ -17,15 +18,45 @@ class Solution {
         I am goin to use 2-d array to represent board, and convert it to 2-d List on the last step only
         */
         
+        int[][] board = new int[n][n];
+        for(int i = 0; i < n; i++) solve(board, i);
+        
         return null;
+    }
+
+    
+
+    public void solve(int[][] board, int startColumn){
+        int n = board.length;
+        if(startColumn == n){
+            printBoard(board);
+            return;
+        }
+        for(int row = 0; row < n; row++){
+                if(isValid(board, row, startColumn)){
+                    if(board[row][startColumn] == 0){
+                        board[row][startColumn] = 1;
+                        solve(board, startColumn + 1);
+                    }
+                }
+        }
+    }
+    
+    // Function to print the board :)
+    public void printBoard(int[][] board){
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board[i].length; j++){
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 
     // Function that is supposed to return true, if we can place Queen in position [row][column]
     public boolean isValid(int[][] board, int row, int column){
-        int size = board.length;
         boolean isValid = true;
-            for(int i = 0; i < size; i++){
-                for(int j = 0; j < size; j++){
+            for(int i = 0; i < board.length; i++){
+                for(int j = 0; j < board.length; j++){
                     if(board[row][j] == 1) // Check if the queen is attacked horizontally
                         isValid = false;
                     if(board[i][column] == 1) //Check if the queen is attacked vertically
