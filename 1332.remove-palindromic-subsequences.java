78706bf7e68fd3bf54a2b8b94555b3499bb9cfc4
GrGrie <gsgrechkin@gmail.com>
2022-06-08 13:49:18 +0800

    Well, I understood that subsequence != substring. I feel stupid now. Nevertheless,  it is a good experience

--- 1332.remove-palindromic-subsequences.java
+++ 1332.remove-palindromic-subsequences.java
@@ -7,32 +7,13 @@
 // @lc code=start
 class Solution {
     public int removePalindromeSub(String s) {
-    
-        int result = 0, i = 0;
-        String tmpString;
-        while(!s.equals("")){
-            tmpString = s.substring(i, s.length());
-            //System.out.println("tmpString before = " + tmpString);
-            if(isPalindrome(tmpString)){
-                //System.out.println("IF HAPPENS! i = " + i);
-                s = s.substring(0, i) + s.substring(i+tmpString.length(), s.length());
-                result++;
-                i = 0;
-                
-            } else {
-                tmpString = s.substring(i, s.length()-1);
-                if(isPalindrome(tmpString)){
-                    s = s.substring(0, i) + s.substring(tmpString.length(), s.length());
-                    result++;
-                    i = 0;
-                    //System.out.println("ELSE HAPPENS!");
-                } else {
-                    i++;
-                }
-            }
-            System.out.println("tmpString after = " + tmpString + " and s = " + s);
-        }
-        return result;
+        
+        /*  Well, I feel like I am stupid, cuz I understood, that
+            subsequence != substring, so we can just remove all 'a's
+            and then remove all 'b's. Feels weird
+        */
+        if(isPalindrome(s)) return 1;
+        else return 2;
 
     }
 
@@ -42,7 +23,6 @@ class Solution {
         for(int i = 0; i < s.length()/2 + 1; i++)
             if(s.charAt(i) != s.charAt(s.length()-i-1)) 
                 isPalindrome = false;
-        //System.out.println("isPalindrome: " + isPalindrome);
         return isPalindrome;
     }
 
