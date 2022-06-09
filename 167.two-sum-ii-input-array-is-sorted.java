6794bdb9fdb2fde785ea76aae1598c41c547cf60
GrGrie <gsgrechkin@gmail.com>
2022-06-09 12:46:48 +0800

    Leetcode Daily Challenge 09.06.2022. Yeah, now, with my second approach I got program that beats 13% runtime and 81% memorywise. Gonna look at discussions and solutions to find a better one. Nevertheless I am proud of myself.

--- 167.two-sum-ii-input-array-is-sorted.java
+++ 167.two-sum-ii-input-array-is-sorted.java
@@ -12,26 +12,46 @@ class Solution {
          * I have to come up with another one. Gonna see what will I figure out
          */
 
+         // Second approach. Now I will use binary search for the second index
         int[] answer = new int[2];
-        boolean isSolved = false;
-        int i = 0;
+        boolean solutionFound = false;
+        int i = 0; // index_1
 
-        while(!isSolved && i < numbers.length-1){
-            int j = i+1;
-            while(j < numbers.length && numbers[j] <= target - numbers[i]){
-                //System.out.println("i = " + i + " and j = " + j);
-                if(numbers[i] + numbers[j] == target){
-                    isSolved = true;
-                    answer[0] = i + 1;
-                    answer[1] = j + 1;
-                    break;
-                }
-                j++;
+        while(!solutionFound){
+            /* 
+             * We know that solution exists and array numbers is sorted. So insted of just
+             * going and searching one by one, we can do a binary search for the second item
+             * For example [2,7,11,15], taget = 9; We first set i = 0 => numbers[i] = 2 =>
+             * lets bs for 9-2 = 7; If we can, we found the solution, if we don't, then just
+             * move i to the next one (i++)
+            */
+            int bs = binarySearch(numbers, target - numbers[i]);
+            // First condition checks whether the binary search was successful or not
+            if(bs != -9999 && i != bs){
+                // If we found values that are equal but have different indexes, my algo gonna skip first
+                // So we have to deal with it by swapping the indexes.
+                if(i > bs){int tmp = i; i = bs; bs = tmp;}
+                answer[0] = i + 1;
+                answer[1] = bs + 1;
+                solutionFound = true;
             }
-            i++;  
+            i++;
         }
-
         return answer;
+    }
+
+    // A simple binary search function
+    private int binarySearch(int[] numbers, int target){
+        int left = 0, right = numbers.length - 1, middle = left + (right-1) / 2;
+        
+        while(left <= right){
+            middle = left + (right-left) / 2; 
+            if(numbers[middle] == target) return middle;
+            if(numbers[middle] < target) left = middle + 1;
+            else right = middle - 1;   
+        }
+        // Value that cannot be achieved in the task, just to show unsuccessful bs
+        return -9999;
     }
 }
 // @lc code=end
