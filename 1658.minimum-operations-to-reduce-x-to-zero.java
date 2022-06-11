4ce0bddaa4bcce940eded001f04b4fb756e8e522
GrGrie <gsgrechkin@gmail.com>
2022-06-11 15:34:55 +0800

    Well, I have read an idea of solution by user GoAheadShaun and implemented it. Feel myself stupid cuz I didn't figure that out myself, even though I had some similar thoughs about it.

--- 1658.minimum-operations-to-reduce-x-to-zero.java
+++ 1658.minimum-operations-to-reduce-x-to-zero.java
@@ -7,47 +7,40 @@
 // @lc code=start
 import java.util.LinkedList;
 class Solution {
-
-    private static int answer = 0;
-
+    //[1,1,4,2,3]\n5
+    //[5,6,7,8,9]\n4
+    //[3,2,20,1,1,3]\n10
+    //[1,1,1,1,1,1,1,1,11,2]\n10
+    //[1,1]\n3
+    //[5,2,3,1,1]\n5
     public int minOperations(int[] nums, int x) {
         /*
          * Leetcode Daily Challenge 11.06.2022. I can see a backtracking solution,
          * but now  I am trying to figure out another one, which would go faster.
+         * I haven't come up with the idea of solution myself, so I have read an article by
+         * "u/GoAheadShaun". I have read only the idea, so now I will try to code it 
          */
-        LinkedList<Integer> numsList = new LinkedList<>();
-        arrToList(nums, numsList);
-        answer = 0;
-        solve(numsList, x);
-        if(answer == 0) answer--;
-        return answer;
-    }
-//[1,1,4,2,3]\n5
-//[5,6,7,8,9]\n4
-//[3,2,20,1,1,3]\n10
-//[1,1,1,1,1,1,1,1,11,2]\n10
-//[1,1]\n3
-    private void solve(LinkedList<Integer> numList, int target){
-        if(target == 0 || (numList.size() == 0 && target != 0)) return;
-            int first = numList.getFirst();
-            int last = numList.getLast();
-            int max = Math.max(first, last);
-            if((max == first && target >= first) || (max == last && target < last && target >= first)){
-                target -= first;
-                numList.removeFirst();
-                answer++;
-                solve(numList, target);
-            } else if((max == last && target >= last) || (max == first && target < first && target >= last)){
-                target -= last;
-                numList.removeLast();
-                answer++;
-                solve(numList, target); 
-            } else return;
+        int answer = -1;
+        int pointerLeft = 0;
+        int target = sumOfArray(nums) - x;
+        int currentSumm = 0;
+        for(int pointerRight = 0; pointerRight < nums.length; pointerRight++){
+            currentSumm += nums[pointerRight];
+            while(currentSumm > target && pointerLeft <= pointerRight){
+                currentSumm -= nums[pointerLeft];
+                pointerLeft++;
+            }
+            if(currentSumm == target) answer = Math.max(answer, pointerRight - pointerLeft + 1);
+        }
+        if(answer == -1) return answer;
+        else return nums.length - answer;
     }
 
-    private void arrToList(int[] nums, LinkedList<Integer> list){
-        for(int i = 0; i < nums.length; i++)
-            list.add(nums[i]);
+    private int sumOfArray(int[] nums){
+        int sum = 0;
+        for(int i = 0; i < nums.length;i++)
+            sum += nums[i];
+        return sum;
     }
 }
 // @lc code=end
