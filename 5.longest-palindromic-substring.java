0a3b3202536a49ee792276d8ba60b6d5447b6972
GrGrie <gsgrechkin@gmail.com>
2022-06-16 14:30:39 +0800

    This is a solution using DP by GoAheahShaun, I fully understtod it but never thought this way. Should dive deeper in DP :)

--- 5.longest-palindromic-substring.java
+++ 5.longest-palindromic-substring.java
@@ -13,46 +13,35 @@ class Solution {
         
         // My tests:
         // civic, level, noon, tacocat, aibohphobia, plan, canal, panama, a, bb, cbc, bcc, square 
-        String answer = "";
-        if(s.length() == 1) return s;
-        int left = 0, right = 0;
-        //Checking odd substrings
-        for(int i = 0; i <= s.length()-1; i++){
-            //Start from the middle of the possible substring and go both left and right
-            left = i; right = i+1;
-            //If it is a polindome -> nice, try to find longer one, based on this
-            while(left >= 0 && right <= s.length() && isPalindrome(s.substring(left, right))){
-                //If our new String is longer than previous answer, update
-                if(answer.length() < s.substring(left, right).length())
-                    answer = s.substring(left, right);
-                //Move to find another polindrome
-                left --;
-                right++;
-            }   
-        }
+        
+        int n = s.length(), left = 0, right = 0;
+        /*
+         * I have read about DP solution from GoAheadShaun, haven't came across this very often, so
+         * I think I should start practising some of this now. Afaiu this is just a matrix that keeps
+         * track whether substring[left][right] is a palilndrome.
+         */
+        boolean[][] dp = new boolean[n][n];
 
-        //Checking even substrings
-        for(int i = 0; i <= s.length()-2; i++){
-            left = i; right = i+2;
-            while(left >= 0 && right <= s.length() && isPalindrome(s.substring(left, right))){
-                if(answer.length() < s.substring(left, right).length())
-                    answer = s.substring(left, right);
-                //Move to find another polindrome
-                left --;
-                right++;
+        for(int length = 0; length < n; length++){
+            for(int i = 0; i + length < n; i++){
+                //Now 'i' is the left pointer and 'i+length' is the right one
+                // If left and right characters are the same, and inner substring is palindrome
+                if(s.charAt(i) == s.charAt(i+length) && (length < 2 || dp[i+1][i+length-1]))
+                // Then current substring is also a palindrome
+                    dp[i][i+length] = true;
+                else
+                // Otherwise it isn't
+                    dp[i][i+length] = false;
+
+                //If current substring is longer then previous maximum, then update maximums
+                if(dp[i][i+length] && length > right - left){
+                    left = i;
+                    right = i + length;
+                }
             }
         }
-
-        return answer;
-    }
-
-    private boolean isPalindrome(String s){
-        boolean isPalindrome = true;
-        for(int i = 0; i < s.length()/2; i++)
-            if(s.charAt(i) != s.charAt(s.length() - i - 1))
-                isPalindrome = false;
+        return s.substring(left, right + 1);
         
-        return isPalindrome;
     }
 }
 // @lc code=end
