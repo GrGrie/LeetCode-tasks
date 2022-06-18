8fec5c980b8e6519c1c39e1113114ef9d7cd6654
GrGrie <gsgrechkin@gmail.com>
2022-06-18 16:33:47 +0800

    Added comments in the code

--- 567.permutation-in-string.java
+++ 567.permutation-in-string.java
@@ -17,17 +17,20 @@ class Solution {
         HashMap<Character, Integer> s2Map = new HashMap<>();
         
         if(s2Len == 0 || s2Len < s1Len) return false;
+        // Count amount of letters in s1 and put letter:amount in the map
         for(int i = 0; i < s1Len; i++) {
             s1Map.putIfAbsent(s1.charAt(i), 0);
             s1Map.put(s1.charAt(i), s1Map.get(s1.charAt(i)) + 1);
         }
+        // Starting from left to right create a windows of length s1Len
         for(int i = 0; i <= s2Len - s1Len; i++){
             String subString = s2.substring(i, i + s1Len);
+            // And then create a similar map but for the substring
             for(int j = 0; j < s1Len; j++){
                 s2Map.putIfAbsent(subString.charAt(j), 0);
                 s2Map.put(subString.charAt(j), s2Map.get(subString.charAt(j)) + 1);
             }
-            System.out.println(s1Map + " | " + s2Map);
+            // If they are equal, then we already can return true
             if(s1Map.equals(s2Map)) return true;
             s2Map.clear();
         }
