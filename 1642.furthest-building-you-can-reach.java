c1d7699965e17697c34f091bb1374b996df796ff
GrGrie <gsgrechkin@gmail.com>
2022-06-21 20:15:58 +0800

    Yeah, this is the solution. I feel really dumb at the moment

--- 1642.furthest-building-you-can-reach.java
+++ 1642.furthest-building-you-can-reach.java
@@ -6,39 +6,29 @@
  */
 
 // @lc code=start
-import java.util.ArrayList;
+import java.util.PriorityQueue;
+import java.util.Collections;
 class Solution {
     /*
      * Leetcode Daily Challenge 21.06.2022
      */
 //[13,9,3,10,11,4,12,1]\n7\n5
     public int furthestBuilding(int[] heights, int bricks, int ladders) {
-        int furthestBuilding = 0;
-        int maxBricksSpent = 0;
-        ArrayList<Integer> bricksSpent = new ArrayList<>();
-        while(ladders >= 0 && furthestBuilding < heights.length){
-            while(bricks > 0 && furthestBuilding < heights.length-1){
-                System.out.println("Ladders = " + ladders + ", bricks = " + bricks);
-                if(heights[furthestBuilding] >= heights[furthestBuilding+1]){
-                    furthestBuilding++;
-                } else {
-                    bricks -= heights[furthestBuilding + 1] - heights[furthestBuilding];
-                    bricksSpent.add(heights[furthestBuilding + 1] - heights[furthestBuilding]);
-                    if(heights[furthestBuilding + 1] - heights[furthestBuilding] > maxBricksSpent)
-                        maxBricksSpent = heights[furthestBuilding + 1] - heights[furthestBuilding];
-                    furthestBuilding++;
-                }
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
+        for (int i=0; i<heights.length-1; i++) {
+            if (heights[i] >= heights[i+1]) continue;
+            bricks -= heights[i+1] - heights[i];
+            pq.add(heights[i+1] - heights[i]);
+
+            if (bricks < 0) {
+                bricks += pq.poll();
+                if (ladders > 0) ladders--;
+                else return i;
             }
-            if(ladders == 0) break;
-            if(maxBricksSpent != 0){
-                bricksSpent.remove(bricksSpent.indexOf(maxBricksSpent));
-                ladders--;
-            }
-            bricks += maxBricksSpent;
         }
-        
-        if(bricks < 0) furthestBuilding--;
-        return furthestBuilding;
+
+        return heights.length - 1;
     }
 }
 // @lc code=end
