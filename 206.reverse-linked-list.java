daef3ee04865f4de09ae6d1a150277ff1eaeb89e
GrGrie <gsgrechkin@gmail.com>
2022-06-24 16:35:12 +0800

    Temporary state of this problem, will come back to it later

---
+++ 206.reverse-linked-list.java
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode id=206 lang=java
+ *
+ * [206] Reverse Linked List
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        
+        var dummy = new ListNode();
+        var answer = dummy;
+
+        while(head != null){
+            break;
+        }
+
+        return answer.next;
+    }
+}
+// @lc code=end
+
